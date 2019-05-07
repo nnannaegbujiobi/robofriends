f2a2d55f93b8a1a6101fd3b1070cffcd3f77f3e1
nnannaegbujiobi <nnanna@newvybe.com>
2019-05-06 19:12:34 -0500

    card compononent

---
+++ src/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+const Card = () => {
+
+  return (
+    
+    <div className='bg-light-green dib br3 pa3 ma2 grow'>
+      <img alt='robots' src='https://robohash.org/test?200x200'/>
+       <div>
+        <h2>Jane Doe</h2>
+        <p>jane.doe@gmail.com</p>
+      </div>
+    </div>
+
+    );
+
+}
+
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello.js';
+import Card from './Card.js';
 
 import * as serviceWorker from './serviceWorker';
 import 'tachyons'
 
-ReactDOM.render(<Hello greeting={'Hello' + 'React Ninja'}/>, document.getElementById('root'));
+ReactDOM.render(
+  
+  <div>
+  <Card/>
+  <Card/>
+  <Card/>
+  </div>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
